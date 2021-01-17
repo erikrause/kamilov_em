097a8f77363498df5c0c745ee7f9c88319354fee
erikrause <erkimkamilov@gmail.com>
2021-01-18 01:35:02 +0500

    init

---
+++ resume_app/controller/Resume.Controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("kamilov_em.controller.App", {
+
+		/*onOpenDialog : function () {
+			this.getOwnerComponent().openHelloDialog();
+		}*/
+	});
+
+});

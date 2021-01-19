41ce1cdcc40bd18643436ca3f4e9871b32ed4d0d
erikrause <erkimkamilov@gmail.com>
2021-01-19 07:08:15 +0500

    Vbox align center.

--- resume_app/controller/Resume.Controller.js
+++ resume_app/controller/Resume.Controller.js
@@ -1,11 +1,25 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	'sap/ui/model/json/JSONModel'
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("kamilov_em.controller.App", {
-
+		
+		onInit : function() {
+				// HTML string bound to the formatted text control
+				var oModel = new JSONModel({
+					HTML : "<p>Знание принципов ООП, сервис-ориентированных и микросервисных архитектур, искусственных нейронных"+
+"сетей, в частности, генеративно-состязательные сети - часть"+
+"выпускной квалификационной работы по генерации пористых структур на основе цифрового керна, по"+
+"данной работе был награжден дипломом 1 степени на студенческой конференции и была представлена к"+
+"публикации соответствующая статья в журнале Вестник кибернетики."+
+"Знание языков программирования: C#, Python, C++, SQL и технологий на платформе .NET. Есть личный"+
+"автомобиль.</p>"
+				});
+				this.getView().setModel(oModel);
+		},
 		/*onOpenDialog : function () {
 			this.getOwnerComponent().openHelloDialog();
 		}*/

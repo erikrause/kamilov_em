4b9efd3ebeff5595332bad83e92b02cbeafd351a
erikrause <erkimkamilov@gmail.com>
2021-01-19 05:21:35 +0500

    Vbox align center.

--- resume_app/controller/Resume.Controller.js
+++ resume_app/controller/Resume.Controller.js
@@ -1,11 +1,27 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	'sap/ui/model/json/JSONModel'
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("kamilov_em.controller.App", {
-
+		
+		onInit : function() {
+				// HTML string bound to the formatted text control
+				var oModel = new JSONModel({
+					HTML : "<h3>subheader</h3>" +
+					"<p>link: <a href=\"//www.sap.com\" style=\"color:green; font-weight:600;\">link to sap.com</a> - links open in a new window.</p>" +
+					"<p>paragraph: <strong>strong</strong> and <em>emphasized</em>.</p>" +
+					"<p>list:</p>" +
+					"<ul><li>list item 1</li><li>list item 2<ul><li>sub item 1</li><li>sub item 2</li></ul></li></ul>" +
+					"<p>pre:</p><pre>abc    def    ghi</pre>" +
+					"<p>code: <code>var el = document.getElementById(\"myId\");</code></p>" +
+					"<p>cite: <cite>a reference to a source</cite></p>" +
+					"<dl><dt>definition:</dt><dd>definition list of terms and descriptions</dd>"
+				});
+				this.getView().setModel(oModel);
+		},
 		/*onOpenDialog : function () {
 			this.getOwnerComponent().openHelloDialog();
 		}*/
